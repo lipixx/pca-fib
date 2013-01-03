8822bfe5266044f0e5ea1b3e1c6890f957846f23
lipixx <lipixx@localhost>
2013-01-03 21:18:38 +0100

    Starting with sqrt vectorization

--- finalProject/sources/3D_Dock/progs/run.sh
+++ finalProject/sources/3D_Dock/progs/run.sh
@@ -2,23 +2,23 @@
 
 make clean
 make
-/usr/bin/time ./ftdock -static 2pka.parsed -mobile 5pti.parsed > /dev/null
-/usr/bin/time ./ftdock -static 1hba.parsed -mobile 5pti.parsed > /dev/null
-/usr/bin/time ./ftdock -static 4hhb.parsed -mobile 5pti.parsed > /dev/null
+/usr/bin/time -p ./ftdock -static 2pka.parsed -mobile 5pti.parsed > /dev/null
+/usr/bin/time -p ./ftdock -static 1hba.parsed -mobile 5pti.parsed > /dev/null
+/usr/bin/time -p ./ftdock -static 4hhb.parsed -mobile 5pti.parsed > /dev/null
 
-make clean
-make g
-/usr/bin/time ./ftdock -static 2pka.parsed -mobile 5pti.parsed > /dev/null
-/usr/bin/time ./ftdock -static 1hba.parsed -mobile 5pti.parsed > /dev/null
-/usr/bin/time ./ftdock -static 4hhb.parsed -mobile 5pti.parsed > /dev/null
+# make clean
+# make g
+# /usr/bin/time ./ftdock -static 2pka.parsed -mobile 5pti.parsed > /dev/null
+# /usr/bin/time ./ftdock -static 1hba.parsed -mobile 5pti.parsed > /dev/null
+# /usr/bin/time ./ftdock -static 4hhb.parsed -mobile 5pti.parsed > /dev/null
 
-make clean
-make pg
-./ftdock -static 2pka.parsed -mobile 5pti.parsed > /dev/null
-gprof ./ftdock > gprof.2pka
-./ftdock -static 1hba.parsed -mobile 5pti.parsed > /dev/null
-gprof ./ftdock > gprof.1hba
-./ftdock -static 4hhb.parsed -mobile 5pti.parsed > /dev/null
-gprof ./ftdock > gprof.4hhb
+# make clean
+# make pg
+# ./ftdock -static 2pka.parsed -mobile 5pti.parsed > /dev/null
+# gprof ./ftdock > gprof.2pka
+# ./ftdock -static 1hba.parsed -mobile 5pti.parsed > /dev/null
+# gprof ./ftdock > gprof.1hba
+# ./ftdock -static 4hhb.parsed -mobile 5pti.parsed > /dev/null
+# gprof ./ftdock > gprof.4hhb
 
 make clean
