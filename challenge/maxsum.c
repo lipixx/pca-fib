27136203fa2709c490c83f1ab63ca174310c5dc3
lipixx <lipixx@localhost>
2013-01-09 12:25:49 +0100

    Primera versi? maxsum ok

--- challenge/maxsum.c
+++ challenge/maxsum.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define max(X, Y) ((X) > (Y) ? (X) : (Y))
+
 int N;
 
 void print_matrix(char matrix[][N]);
-void read_matrix(char matrix[][N],int n);
+void read_matrix(char matrix[][N]);
+int maxsum(char matrix[][N]);
 
 int main(int argc, char *argv[])
 {
@@ -13,13 +16,14 @@ int main(int argc, char *argv[])
   scanf("%i",&N);
   char (*matrix)[N] = malloc(sizeof *matrix * N);
 
-  read_matrix(matrix,N);
-  print_matrix(matrix);
+  read_matrix(matrix);
+  printf("%i",maxsum(matrix));
+  /*print_matrix(matrix);*/
 
   return 0;
 }
 
-void read_matrix(char matrix[][N],int n)
+void read_matrix(char matrix[][N])
 {
   int i,j,r;
 
@@ -46,4 +50,29 @@ void print_matrix(char matrix[][N])
       printf("\n");
     }
   printf("##END OF MATRIX PRINT##\n");
+}
+
+int maxsum(char matrix[][N])
+{
+  int x,y,i,j,acum,tmp,result;
+  
+  result = 0xFFFFFFFF;
+
+  for (x=0; x<N; x++)
+    for (y=0; y<N; y++)
+      {
+	acum = 0xFFFFFFFF;
+	for (i=0;i<N;i++)
+	  {
+	    tmp = 0;
+	    for (j=x;j<=y;j++)
+	      {
+		tmp += matrix[i][j];
+	      }
+	    acum = max(acum + tmp, tmp);
+	    result = max(result, acum);
+	  }
+      }
+  
+  return result;
 }
