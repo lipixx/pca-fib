c8d38ed381dcbcdfb22ce3949c329d18dd12ee99
lipixx <lipixx@localhost>
2013-01-09 15:25:15 +0100

    Updated parameters

--- challenge/maxsum.c
+++ challenge/maxsum.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define Dif(i,j,k) (matrix[i+k][j] - matrix[i][j])
+#define SUB(i,j,k) (matrix[i+k][j] - matrix[i][j])
 #define MAXN 810
 
-int N, MAX;
+int N;
 int matrix[MAXN][MAXN];
 
 void read_matrix();
@@ -51,28 +51,28 @@ void read_matrix()
 
 int maxsum()
 {
-  int i, j, k, t;
+  int i, j, k, x, result;
   for(i = 1; i<=N; i++)
     {
       for(j = 0; j<N; j++)
 	matrix[i][j] =  matrix[i][j] + matrix[i-1][j];
     }
 
-  MAX = matrix[1][0];
+  result = matrix[1][0];
   for(k = 1; k<=N; k++)
     {
       for(i = 0; i<=N-k; i++)
 	{
-	  for(t = 0, j = 0; j<N; j++)
+	  for(x = 0, j = 0; j<N; j++)
 	    {
-	      if(t>=0)
-		t+= Dif(i,j,k);
+	      if(x >= 0)
+		x += SUB(i,j,k);
 	      else
-		t = Dif(i,j,k);
-	      if(t>MAX)
-		MAX = t;
+		x = SUB(i,j,k);
+	      if(x > result)
+		result = x;
 	    }
 	}
     }               
-  return MAX;
+  return result;
 }
